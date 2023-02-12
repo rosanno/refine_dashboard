a7bbe6b78e6b867408ca3eb45dc7ba1a90054bc3
rosanno <rosannomacabuhay@gmail.com>
2023-02-12 12:20:43 +0800

    first commit

---
+++ client/src/pages/index.ts
@@ -0,0 +1,21 @@
+import AgentProfile from "./agent-profiel";
+import Agent from "./agent";
+import AllProperties from "./all-properties";
+import CreateProperty from "./create-property";
+import Home from "./home";
+import { Login } from "./login";
+import MyProfile from "./my-profile";
+import PropertyDetails from "./property-details";
+import EditProperty from "./edit-property";
+
+export {
+  AgentProfile,
+  Agent,
+  AllProperties,
+  CreateProperty,
+  Home,
+  Login,
+  MyProfile,
+  PropertyDetails,
+  EditProperty,
+};
